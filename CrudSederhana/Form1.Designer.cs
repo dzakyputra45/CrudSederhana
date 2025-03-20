216582c4c8c86716dfa5fe02f44f163af7387a2d
dzakyputra45 <dzakypratama45@gmail.com>
2025-03-20 23:59:24 +0700

    Add project files.

---
+++ CrudSederhana/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace CrudSederhana
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNAMA = new System.Windows.Forms.TextBox();
+            this.btnTAMBAH = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(160, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "NAMA";
+            // 
+            // txtNAMA
+            // 
+            this.txtNAMA.Location = new System.Drawing.Point(235, 54);
+            this.txtNAMA.Name = "txtNAMA";
+            this.txtNAMA.Size = new System.Drawing.Size(100, 22);
+            this.txtNAMA.TabIndex = 1;
+            // 
+            // btnTAMBAH
+            // 
+            this.btnTAMBAH.Location = new System.Drawing.Point(426, 54);
+            this.btnTAMBAH.Name = "btnTAMBAH";
+            this.btnTAMBAH.Size = new System.Drawing.Size(75, 23);
+            this.btnTAMBAH.TabIndex = 2;
+            this.btnTAMBAH.Text = "TAMBAH";
+            this.btnTAMBAH.UseVisualStyleBackColor = true;
+            this.btnTAMBAH.Click += new System.EventHandler(this.btnTAMBAH_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnTAMBAH);
+            this.Controls.Add(this.txtNAMA);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNAMA;
+        private System.Windows.Forms.Button btnTAMBAH;
+    }
+}
+
